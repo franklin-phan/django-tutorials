560003715a3635fc860449db9fd977168e8ca68c
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-02-13 23:02:16 -0800

    fourth step

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 #we need to tell the admin that Question objects have an admin interface
-from .models import Question
+from .models import Question, Choice
 admin.site.register(Question)
+admin.site.register(Choice)
 # Register your models here.
